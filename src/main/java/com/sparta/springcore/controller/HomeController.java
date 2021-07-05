8683bba5fa326f1c6f156b3f2101d525c4b7024d
이중원 <>
2021-07-06 04:49:07 +0900

    4-12 폴더 생성 기능(Server)

--- src/main/java/com/sparta/springcore/controller/HomeController.java
+++ src/main/java/com/sparta/springcore/controller/HomeController.java
@@ -1,14 +1,27 @@
 package com.sparta.springcore.controller;
 
+import com.sparta.springcore.model.Folder;
 import com.sparta.springcore.security.UserDetailsImpl;
+import com.sparta.springcore.service.FolderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    private final FolderService folderService;
+
+    @Autowired
+    public HomeController(FolderService folderService) {
+        // 멤버 변수 생성
+        this.folderService = folderService;
+    }
+
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {     // @AuthenticationPrincipal 통해서 로그인 된 사람의 정보를 자동으로 넘겨줌
         model.addAttribute("username", userDetails.getUsername());                  // thymeleaf를 통해서 username 전달되어 줌
