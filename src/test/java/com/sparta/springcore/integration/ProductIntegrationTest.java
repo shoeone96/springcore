95de70718751f707a59bc8c5e8bf479d1d1514f1
이중원 <>
2021-07-05 11:07:18 +0900

    4-4(페이징 구현 오류 수정검사 전)

--- src/test/java/com/sparta/springcore/integration/ProductIntegrationTest.java
+++ src/test/java/com/sparta/springcore/integration/ProductIntegrationTest.java
@@ -81,7 +81,7 @@ class ProductIntegrationTest {
     void test3() {
         // given
         // when
-        List<Product> productList = productService.getProducts(userId);
+        List<Product> productList = productService.getProducts(userId, page, size, sortBy, isAsc);
 
         // then
         // 1. 전체 상품에서 테스트에 의해 생성된 상품 찾아오기 (상품의 id 로 찾음)
